9d95496183634e71843f5e449254bee02561de64
Wilfried Goesgens <dothebart@citadel.org>
2018-02-24 15:29:38 +0100

    abort if we loose connection to the grovepi, so we have a chance to reset it.

--- Projects/MonitorEnvironment/monitor_environment.py
+++ Projects/MonitorEnvironment/monitor_environment.py
@@ -41,22 +41,23 @@ import grovepi
 import atexit
 
 # from grovepi import *
-from grove_rgb_lcd import *
+#from grove_rgb_lcd import *
 from math import isnan
 
 atexit.register(grovepi.dust_sensor_dis)
 
 is_more_silent = 0
-loudness_sensor = 0
+loudness_sensor = 2
 # connect loudness senor to port A2
 
-grovepi.pinMode(loudness_sensor,"INPUT")
+# xxx grovepi.pinMode(loudness_sensor,"INPUT")
 
-threshhold = 140
+threshhold = 300
+count_loud = 0
 
 # connect dust sensor to port D2
 print("Reading from the dust sensor")
-# grovepi.dust_sensor_en()
+grovepi.dust_sensor_en()
 
 dht_sensor_port = 7 # connect the DHt sensor to port 7
 dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colored sensor
@@ -66,7 +67,7 @@ dht_sensor_type = 0 # use 0 for the blue-colored sensor and 1 for the white-colo
 # SIG,NC,VCC,GND
 air_sensor = 0
 
-grovepi.pinMode(air_sensor,"INPUT")
+# grovepi.pinMode(air_sensor,"INPUT")
 
 # Connect the Relais port to D4
 # SIG,NC,VCC,GND
@@ -77,30 +78,60 @@ grovepi.pinMode(relais_port,"OUTPUT")
 
 zabbix_host="192.168.3.111"
 
-
-
 while True:
     try:
+        # gather the max for 5 seconds:
+        loudness = 0;
+        count = 0
+        avg = 0;
+        while count < 16:
+            val = grovepi.analogRead(loudness_sensor)
+            if val > 65000:
+                continue
+            if (val > loudness):
+                loudness = val
+            avg += val
+	    time.sleep(0.3)
+            count += 1
+        avg /= count
+        # count the rounds of roaring / silence
+        if loudness > threshhold:
+            count_loud += 1
+            is_more_silent = 0
+        else:
+            is_more_silent += 1
+            count_loud = 0;
+
+        print("noise = %d avg = %d loud = %d silent = %d" % (loudness, avg, count_loud, is_more_silent))
+        # what? 25 seconds noise? Disco!
+        if count_loud > 5:
+	    print("x"*80)
+            grovepi.digitalWrite(relais_port, 1)
+
+        # silent again for > 10 seconds? go dark again.
+        if is_more_silent > 2:
+            grovepi.digitalWrite(relais_port, 0)
+
         # the PM comes at its own frequency, so we send it alone.
-	# [new_val,lowpulseoccupancy] = grovepi.dustSensorRead()
-	# if new_val:
-	#     print(lowpulseoccupancy)
-        #     ''' create DataContainer, providing data_type, zabbix server and port '''
-        #     zbx_container = protobix.DataContainer("items", zabbix_host, 10051)
-        #     ''' set debug '''
-        #     zbx_container.set_debug(True)
-        #     zbx_container.set_verbosity(True)
-        # 
-        #     zbx_container.add_item("grooveberry", "my.zabbix.pm-count", lowpulseoccupancy)
-	# 	
-	#     ''' Send data to zabbix '''
-	#     ret = zbx_container.send(zbx_container)
-	#     ''' If returns False, then we got a problem '''
-	#     if not ret:
-	# 	print "Ooops. Something went wrong when sending data to Zabbix"
-        #     else:
-        #         zbx_container.item_list = []
-        # 
+	[new_val,lowpulseoccupancy] = grovepi.dustSensorRead()
+	if new_val:
+	    print(lowpulseoccupancy)
+            #''' create DataContainer, providing data_type, zabbix server and port '''
+            #zbx_container = protobix.DataContainer("items", zabbix_host, 10051)
+            #''' set debug '''
+            #zbx_container.set_debug(True)
+            #zbx_container.set_verbosity(True)
+            #
+            #zbx_container.add_item("grooveberry", "my.zabbix.pm-count", lowpulseoccupancy)
+	    #    
+	    #''' Send data to zabbix '''
+	    #ret = zbx_container.send(zbx_container)
+	    #''' If returns False, then we got a problem '''
+	    #if not ret:
+	    #    print "Ooops. Something went wrong when sending data to Zabbix"
+            #else:
+            #    zbx_container.item_list = []
+        
         # get the temperature and Humidity from the DHT sensor
         [ temp,hum ] = grovepi.dht(dht_sensor_port,dht_sensor_type)
         print("temp =", temp, "C\thumidity =", hum,"%")
@@ -109,38 +140,21 @@ while True:
         # if so, then raise a type error exception
         if isnan(temp) is True or isnan(hum) is True:
             raise TypeError('nan error')
-
+        
         t = str(temp)
         h = str(hum)
 
         # Get the air quality sensor value
-        #sensor_value = grovepi.analogRead(air_sensor)[2]
-        sensor_value = 0
+        sensor_value = grovepi.analogRead(air_sensor)
         val = "Air fresh"
-        #if sensor_value > 700:
-        #    val = "High pollution"
-        #elif sensor_value > 300:
-        #    val = "Low pollution"
-        #
-        #print("sensor_value =", sensor_value)
+        if sensor_value > 700:
+            val = "High pollution"
+        elif sensor_value > 300:
+            val = "Low pollution"
         
-        loudness = 0;
-        count = 0
-        while count < 500:
-            val = grovepi.analogRead(loudness_sensor)[2]
-            if (val > loudness):
-                loudness = val
-	    time.sleep(0.01)
-            count += 1
-        if loudness > threshhold:
-            grovepi.digitalWrite(relais_port, 1)
-            is_more_silent = 0
-        else:
-            is_more_silent += 1
-            if is_more_silent > 2:
-                grovepi.digitalWrite(relais_port, 1)
+        print("sensor_value =", sensor_value)
 
-        if 1:
+        if 0:
             ''' create DataContainer, providing data_type, zabbix server and port '''
             zbx_container = protobix.DataContainer("items", zabbix_host, 10051)
             ''' set debug '''
@@ -166,9 +180,13 @@ while True:
                 zbx_container.item_list = []
 
 
-    except IOError:
-        print ("Error")
-
+    except IOError as ex:
+        print ("Error %s" % str(ex))
+	if (str(ex) == "121"): 
+	  raise ex
+    except TypeError as ex: 
+        print ("Error %s" % str(ex))
+        raise ex
 
 
 print "Everything is OK"
